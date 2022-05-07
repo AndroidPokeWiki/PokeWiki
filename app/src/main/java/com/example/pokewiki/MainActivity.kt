723fb0af10ca6101c99ab1eea3026c5e3c5dd7fa
bitterlemon <873811906@qq.com>
2022-05-07 16:58:18 +0800

    add use kotlin

---
+++ app/src/main/java/com/example/pokewiki/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pokewiki
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
+    }
+}
