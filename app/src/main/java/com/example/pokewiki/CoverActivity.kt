dba315b73cf9d387fa0cc14f52e62cb3ff0b951c
bitterlemon <873811906@qq.com>
2022-05-08 21:02:41 +0800

    cover activity finish

---
+++ app/src/main/java/com/example/pokewiki/CoverActivity.kt
@@ -0,0 +1,22 @@
+package com.example.pokewiki
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pokewiki.login.LoginActivity
+
+class CoverActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setContentView(R.layout.cover_activity)
+        super.onCreate(savedInstanceState)
+
+        initAction()
+    }
+
+    private fun initAction(){
+        Thread{
+            Thread.sleep( 3000)
+            startActivity(Intent(this, LoginActivity::class.java))
+        }.start()
+    }
+}
--- app/src/main/java/com/example/pokewiki/login/LoginActivity.kt
+++ app/src/main/java/com/example/pokewiki/login/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.example.pokewiki
+package com.example.pokewiki.login
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.pokewiki.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_activity)
         super.onCreate(savedInstanceState)
     }
 }
