b94c60bbd320d5aa320064a559fc197ea6b0f177
bitterlemon <873811906@qq.com>
2022-06-05 14:41:38 +0800

    cover counter and about page finish

--- app/src/main/java/com/example/pokewiki/CoverActivity.kt
+++ app/src/main/java/com/example/pokewiki/CoverActivity.kt
@@ -1,28 +1,57 @@
 package com.example.pokewiki
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pokewiki.login.LoginActivity
+import com.ruffian.library.widget.RTextView
 import qiu.niorgai.StatusBarCompat
 
 class CoverActivity : AppCompatActivity() {
+    private lateinit var mSkipBtn: RTextView
+    private lateinit var mCounter: CountDownTimer
+    private var ms = 4
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setContentView(R.layout.cover_activity)
         super.onCreate(savedInstanceState)
 
-        StatusBarCompat.translucentStatusBar(this)
+        StatusBarCompat.setStatusBarColor(this, Color.BLACK)
         window.navigationBarColor = Color.BLACK
+
+        mSkipBtn = findViewById(R.id.cover_skip)
+        mSkipBtn.setOnClickListener {
+            mCounter.cancel()
+            myFinish()
+        }
+
         initAction()
     }
 
-    private fun initAction(){
-        Thread{
-            Thread.sleep( 3000)
+    private fun initAction() {
+        mCounter = object : CountDownTimer(3000, 1000) {
+            @SuppressLint("SetTextI18n")
+            override fun onTick(millisUntilFinished: Long) {
+                ms -= 1
+                mSkipBtn.text = "跳过 $ms s"
+            }
+
+            override fun onFinish() {
+                myFinish()
+            }
+        }
+        mCounter.start()
+    }
+
+    private fun myFinish() {
+
+        val type = intent.getStringExtra("type")
+        if (type == null)
             startActivity(Intent(this, LoginActivity::class.java))
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-            finish()
-        }.start()
+        finish()
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
     }
 }
--- app/src/main/java/com/example/pokewiki/main/about/AboutFragment.kt
+++ app/src/main/java/com/example/pokewiki/main/about/AboutFragment.kt
@@ -1,23 +1,48 @@
 package com.example.pokewiki.main.about
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
+import com.example.pokewiki.CoverActivity
 import com.example.pokewiki.R
+import com.example.pokewiki.main.profile.advice.AdviceActivity
+import com.example.pokewiki.utils.ToastUtils
 
-class AboutFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.info_fragment, container, false)
-    }
+class AboutFragment : Fragment(R.layout.info_fragment) {
+    private lateinit var mCheckUpdateBtn: CardView
+    private lateinit var mLikeAppBtn: CardView
+    private lateinit var mWelcomeBtn: CardView
+    private lateinit var mFeedbackBtn: CardView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView(view)
+    }
+
+    private fun initView(view : View) {
+        mCheckUpdateBtn = view.findViewById(R.id.about_check_update)
+        mCheckUpdateBtn.setOnClickListener {
+            ToastUtils.getInstance(requireContext())?.showShortToast("当前已经是最新版本")
+        }
+        mLikeAppBtn = view.findViewById(R.id.about_like)
+        mLikeAppBtn.setOnClickListener {
+            ToastUtils.getInstance(requireContext())?.showShortToast("感谢支持！！")
+        }
+        mWelcomeBtn = view.findViewById(R.id.about_welcome)
+        mWelcomeBtn.setOnClickListener {
+            startActivity(
+                Intent(requireContext(), CoverActivity::class.java).putExtra(
+                    "type",
+                    "welcome"
+                )
+            )
+            requireActivity().overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }
+        mFeedbackBtn = view.findViewById(R.id.about_feedback)
+        mFeedbackBtn.setOnClickListener {
+            startActivity(Intent(requireContext(), AdviceActivity::class.java))
+        }
     }
 }
